343a355b25f7b5bb86e777a1b920d8d51cfdc15f
samsonan <andreynsamsonov@gmail.com>
2016-09-28 19:39:21 +0300

    initial commit

---
+++ src/main/java/com/samsonan/bplaces/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.samsonan.bplaces.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getIndexPage(Model model) {
+        model.addAttribute("name", "Andrey");
+        return "home";
+    }
+
+}
