708bdbb6198fd8da2fb82375f17b8420bce81f1d
Andrei Samsonov <ASamsonov@luxoft.com>
2016-09-30 20:54:55 +0300

    integration tests foundation

--- src/test/java/com/samsonan/bplaces/test/ControllerTest.java
+++ src/test/java/com/samsonan/bplaces/test/ControllerTest.java
@@ -1,16 +1,10 @@
 package com.samsonan.bplaces.test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.equalTo;
 
-import org.springframework.boot.test.json.JsonContentAssert;
-
-import static org.junit.Assert.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.nio.charset.Charset;
 
@@ -19,21 +13,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.ExpectedDatabase;
 import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@DatabaseSetup("/bplaces_places_init.xml")
+@DatabaseSetup("/bplaces_init.xml")
 @AutoConfigureMockMvc
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+    TransactionalTestExecutionListener.class,
+    DbUnitTestExecutionListener.class })
+@ActiveProfiles("test")
 public class ControllerTest {
 
     @Autowired
@@ -59,12 +62,19 @@ public class ControllerTest {
     }
     
     @Test
-    @ExpectedDatabase(value = "/bplaces_places_init.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)        
+    @ExpectedDatabase(value = "/bplaces_init.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)        
     public void getAllUsers() throws Exception {
+        
         mockMvc.perform(get("/places/"))
         .andExpect(status().isOk())
         .andExpect(content().contentType(contentType))
-        .andExpect(content().string(""));
+        .andExpect(jsonPath("$", hasSize(2)))
+        .andExpect(jsonPath("$[0].name", is("First Place")))
+        .andExpect(jsonPath("$[0].latitude", is(102.123456)))
+        .andExpect(jsonPath("$[0].longitude", is(-102.123456)))
+        .andExpect(jsonPath("$[1].name", is("~*[];'{}()?.,")))
+        .andExpect(jsonPath("$[1].latitude", is(-0.654321)))
+        .andExpect(jsonPath("$[1].longitude", is(0.123456)));
     }
     
 }
